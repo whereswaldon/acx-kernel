a1af21d484df7a58ffecbac36c6e009077b5503a
Waldon <waldonck@appd.appstate.edu>
2016-01-28 14:59:16 -0500

    Initial commit.

---
+++ ACX-kernel/main.c
@@ -0,0 +1,18 @@
+/*
+ * ACX-kernel.c
+ *
+ * Created: 1/28/2016 2:56:40 PM
+ * Author : waldonck
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
